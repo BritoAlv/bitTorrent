7c9809de887e794f5304ca0e8f3bf72289c45864
Javier Lima <javi.limashelby@gmail.com>
2025-02-15 11:52:26 -0500

    feat: Set gui-frontend infrastructure

---
+++ src/gui/frontend/host.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def home():
+    return send_file("./static/index.html")
+
+@app.route('/index', methods = ['GET'])
+def index():
+    return home()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
