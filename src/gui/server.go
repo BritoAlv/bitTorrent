a36d3eb5b3111bd9a3621a43faa0975adb36c24b
Javier Lima <javi.limashelby@gmail.com>
2025-02-28 11:03:41 -0500

    refactor: Integrate hosting and API into a single process

--- src/gui/server.go
+++ src/gui/server.go
@@ -2,73 +2,187 @@ package main
 
 import (
 	"bittorrent/client/peer"
-	"bittorrent/gui/backend"
+	"bittorrent/torrent"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
-	"sync"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		hostSite()
-	}()
-
-	go func() {
-		defer wg.Done()
-		// deployApi()
-	}()
-
-	wg.Wait()
-}
-
-func hostSite() {
 	router := gin.Default()
 
-	router.Static("/", "./frontend/static")
-	router.Static("/index", "./frontend/static")
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Allow any origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-	router.Run(":9090")
+	// Setup API
+	handler := newApiHandler()
+	router.POST("/download", handler.download)
+	router.GET("/update/:id", handler.update)
+	router.GET("/kill/:id", handler.kill)
+
+	// Host files
+	router.Static("/home", "./static")
+
+	err := router.Run(":9090")
+	if err != nil {
+		log.Println("Server could not be started")
+	}
 }
 
-func deployApi() {
-	// Create a new request multiplexer
-	// Take incoming requests and dispatch them to the matching handlers
-	requestMultiplexer := http.NewServeMux()
-	peers := make(map[string]*peer.Peer)
-
-	// Register the routes and handlers
-	requestMultiplexer.Handle("/download", &backend.DownloadHandler{Peers: peers})
-	requestMultiplexer.Handle("/update", &backend.UpdateHandler{Peers: peers})
-	requestMultiplexer.Handle("/kill", &backend.KillHandler{Peers: peers})
-
-	// Wrap the multiplexer with a CORS handler
-	handlerWithCORS := corsMiddleware(requestMultiplexer)
-
-	fmt.Println("Torrente API started")
-	// Run the server
-	http.ListenAndServe("127.0.0.1:9595", handlerWithCORS)
+// ** Contracts
+type downloadRequest struct {
+	Id              string
+	TorrentPath     string
+	DownloadPath    string
+	IpAddress       string
+	EncryptionLevel bool
 }
 
-// CORS middleware
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")                   // Allow any origin
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS") // Allowed methods
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")       // Allowed headers
+type booleanResponse struct {
+	Successful   bool
+	ErrorMessage string
+}
 
-		// Handle preflight OPTIONS request
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNoContent) // Respond with 204 No Content
-			return
-		}
+type updateResponse struct {
+	booleanResponse
+	Progress float32
+	Peers    int
+}
 
-		// Pass request to the next handler
-		next.ServeHTTP(w, r)
+// ** Handler definition and implementation
+type apiHandler struct {
+	Peers map[string]*peer.Peer
+}
+
+func newApiHandler() *apiHandler {
+	return &apiHandler{
+		Peers: make(map[string]*peer.Peer),
+	}
+}
+
+func (handler *apiHandler) download(ctx *gin.Context) {
+	var request downloadRequest
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		ctx.IndentedJSON(http.StatusBadRequest, booleanResponse{
+			Successful:   false,
+			ErrorMessage: err.Error(),
+		})
+		return
+	}
+
+	_peer, err := startPeer(request.Id, request.TorrentPath, request.DownloadPath, request.IpAddress, request.EncryptionLevel)
+	if err != nil {
+		fmt.Println("API: Error: " + err.Error())
+		ctx.IndentedJSON(http.StatusInternalServerError, booleanResponse{
+			Successful:   false,
+			ErrorMessage: err.Error(),
+		})
+		return
+	}
+
+	go _peer.Torrent(nil)
+	handler.Peers[request.Id] = _peer
+
+	ctx.IndentedJSON(http.StatusOK, booleanResponse{
+		Successful:   true,
+		ErrorMessage: "",
 	})
+}
+
+func (handler *apiHandler) update(ctx *gin.Context) {
+	id, found := ctx.Params.Get("id")
+	if !found {
+		ctx.IndentedJSON(http.StatusBadRequest, updateResponse{
+			booleanResponse: booleanResponse{
+				Successful:   false,
+				ErrorMessage: "Expecting 'id' query",
+			},
+			Progress: 0,
+			Peers:    0,
+		})
+		return
+	}
+
+	_peer, contained := handler.Peers[id]
+	if !contained {
+		ctx.IndentedJSON(http.StatusNotFound, updateResponse{
+			booleanResponse: booleanResponse{
+				Successful:   false,
+				ErrorMessage: "Non existing 'id'",
+			},
+			Progress: 0,
+			Peers:    0,
+		})
+		return
+	}
+
+	progress, peers := _peer.Status()
+
+	ctx.IndentedJSON(http.StatusOK, updateResponse{
+		booleanResponse: booleanResponse{
+			Successful:   true,
+			ErrorMessage: "",
+		},
+		Progress: progress,
+		Peers:    peers,
+	})
+}
+
+func (handler *apiHandler) kill(ctx *gin.Context) {
+	id, found := ctx.Params.Get("id")
+	if !found {
+		ctx.IndentedJSON(http.StatusBadRequest, booleanResponse{
+			Successful:   false,
+			ErrorMessage: "Expecting 'id' query",
+		})
+		return
+	}
+
+	_peer, contained := handler.Peers[id]
+	if !contained {
+		ctx.IndentedJSON(http.StatusNotFound, booleanResponse{
+			Successful:   false,
+			ErrorMessage: "Non existing 'id'",
+		})
+		return
+	}
+
+	_peer.NotificationChannel <- peer.KillNotification{}
+	delete(handler.Peers, id)
+
+	ctx.IndentedJSON(http.StatusOK, booleanResponse{
+		Successful:   true,
+		ErrorMessage: "",
+	})
+}
+
+func startPeer(id string, torrentPath string, downloadPath string, ip string, encryptionLevel bool) (*peer.Peer, error) {
+	torrent, err := torrent.ParseTorrentFile(torrentPath)
+	if err != nil {
+		return nil, err
+	}
+
+	listener, err := net.Listen("tcp", ip+":")
+	if err != nil {
+		return nil, err
+	}
+
+	_peer, err := peer.New(id, listener, torrent, downloadPath, encryptionLevel)
+	if err != nil {
+		return nil, err
+	}
+
+	return &_peer, nil
 }
